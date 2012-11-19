c3432a4e956d6f6db85e4f71338a8a7c4249a413
Prasan <ZenBowman@gmail.com>
2012-11-18 17:44:42 -0800

    Beginning impl of red black trees

---
+++ RedBlackTree.py
@@ -0,0 +1,13 @@
+## 11/18/2012
+## Prasan Samtani
+## License: GPL
+
+class RBNode:
+    def __init__ (self, color, key, left, right, p):
+        self.color = color
+        self.key = key
+        self.left = left
+        self.right = right
+        self.p = p
+
+
