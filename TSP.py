4a99e6e7507dc7f8027a3bff800a418b4d5f00c1
ZenBowman <PrasanSamtani@gmail.com>
2012-12-02 13:22:39 -0800

    Modified TSP.py, added nearest neighbor algorithm

--- TSP.py
+++ TSP.py
@@ -36,11 +36,44 @@ def plotsolution(data, soln):
         ys.append(positem[1])
     plt.plot(xs, ys)
     
+def dsqr(data, k1, k2):
+    p1 = data[k1]
+    p2 = data[k2]
+    ds = pow(p2[0]-p1[0], 2) + pow(p2[1] - p1[1],2)
+    return ds
 
+def nearestneighbor(data, startnode):
+    unvisited = set(data.keys())
+    visited = set()
+    tour = [startnode]
+    cpos = startnode
+    unvisited.remove(startnode)
+    while len(unvisited) > 0:
+        nn = None
+        mindist = 0
+        # Find nearest neighbor
+        for item in unvisited:
+            if nn is None:
+                nn = item
+                mindist = dsqr(data, cpos, item)
+            else:
+                thisdist = dsqr(data, cpos, item)
+                if thisdist < mindist:
+                    mindist = thisdist
+                    nn = item
+        # Set curpos to nn, remove nn from unvisited
+        tour.append(nn)
+        unvisited.remove(nn)
+        visited.add(nn)
+        cpos = nn
+    tour.append(startnode)
+    return tour
+        
 
 if __name__ == "__main__":
     d = generateTSP(8)
     print d
     plotTSP(d)
-    plotsolution(d, ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'A'])
+    nnsol = nearestneighbor(d, 'A') 
+    plotsolution(d,nnsol)
     plt.show()
